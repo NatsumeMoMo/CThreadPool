94aeb5be1fbbdf614e427b12cfb3c196e706a290
JasoBot <88011368+JasoBot@users.noreply.github.com>
2023-12-21 11:18:01 +0800

    Add files via upload

--- Cthreadpool.cpp
+++ Cthreadpool.cpp
@@ -1,12 +1,13 @@
-#include"Cthreadpool.h"
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<windows.h>
-#include<unistd.h>
-//创建线程池
-Cthreadpool* createThreadPool(int minThreadnum, int maxThreadnum, int taskmax){
-    Cthreadpool* pool = (Cthreadpool*)malloc(sizeof(Cthreadpool));
+#include "Cthreadpool.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+#include <unistd.h>
+// 创建线程池
+Cthreadpool *createThreadPool(int minThreadnum, int maxThreadnum, int taskmax)
+{
+    Cthreadpool *pool = (Cthreadpool *)malloc(sizeof(Cthreadpool));
 
     /*
     将以下逻辑代码放入到一个 do-while 循环中是因为一开始给 pool 分配了堆内存
@@ -17,47 +18,51 @@ Cthreadpool* createThreadPool(int minThreadnum, int maxThreadnum, int taskmax){
     */
     do
     {
-        if(pool==nullptr){
+        if (pool == nullptr)
+        {
             printf("Create ThreadPool failed !\n");
-            break;//return NULL;
+            break; // return NULL;
         }
 
-        pool->ThreadWorkers=(HANDLE*)malloc(maxThreadnum*sizeof(HANDLE));
-        if(pool->ThreadWorkers==nullptr){
+        pool->ThreadWorkers = (HANDLE *)malloc(maxThreadnum * sizeof(HANDLE));
+        if (pool->ThreadWorkers == nullptr)
+        {
             printf("Failed to allocate memory to ThreadWorker !\n");
-            break;//return NULL;
+            break; // return NULL;
         }
-        memset(pool->ThreadWorkers,NULL,maxThreadnum*sizeof(HANDLE));//必须是初始化 maxThreadnum 个 sizof(HANDLE) 大小的空间
+        memset(pool->ThreadWorkers, NULL, maxThreadnum * sizeof(HANDLE)); // 必须是初始化 maxThreadnum 个 sizof(HANDLE) 大小的空间
 
-        pool->ThreadWorkersID=(DWORD*)malloc(maxThreadnum*sizeof(DWORD));
-        if(pool->ThreadWorkersID==nullptr){
+        pool->ThreadWorkersID = (DWORD *)malloc(maxThreadnum * sizeof(DWORD));
+        if (pool->ThreadWorkersID == nullptr)
+        {
             printf("Failed to allocate memory to ThreadWorkerID !\n");
             break;
         }
-        pool->MinThreadnum=minThreadnum;
-        pool->MaxThreadnum=maxThreadnum;
-        pool->IsDestoryPool=0;
-        pool->WorkingThreadnum=0;
-        pool->liveThreadnum=minThreadnum; //刚开始按照最小个数创建
-        pool->NeedtoKillnum=0;
+        pool->MinThreadnum = minThreadnum;
+        pool->MaxThreadnum = maxThreadnum;
+        pool->IsDestoryPool = 0;
+        pool->WorkingThreadnum = 0;
+        pool->liveThreadnum = minThreadnum; // 刚开始按照最小个数创建
+        pool->NeedtoKillnum = 0;
 
-        pool->Taskmax=taskmax;
-        pool->CurrnetTasknum=0;
-        pool->front=0;
-        pool->rear=0;
-        pool->TaskQueue=(Task*)malloc(pool->Taskmax*sizeof(Task));
+        pool->Taskmax = taskmax;
+        pool->CurrnetTasknum = 0;
+        pool->front = 0;
+        pool->rear = 0;
+        pool->TaskQueue = (Task *)malloc(pool->Taskmax * sizeof(Task));
 
-        InitializeCriticalSection(&pool->mutexPool);//没有返回值
+        InitializeCriticalSection(&pool->mutexPool); // 没有返回值
         InitializeCriticalSection(&pool->mutexWorkingnum);
         InitializeConditionVariable(&pool->isTaskEmpty);
         InitializeConditionVariable(&pool->isTaskFull);
 
-        pool->ThreadManger=CreateThread(NULL,0,ThreadMangerFunction,pool,0,&(pool->ThreadMangerID));
-        for(int i=0;i<minThreadnum;i++){
-            //为每个工作线程分配一个唯一的线程标识符
+        pool->ThreadManger = CreateThread(NULL, 0, ThreadMangerFunction, pool, 0, &(pool->ThreadMangerID));
+        for (int i = 0; i < minThreadnum; i++)
+        {
+            // 为每个工作线程分配一个唯一的线程标识符
             /*
             之所以传入 pool 作为参数，原因是工作线程的主函数需要访问和操作线程池
-            的状态，例如从任务队列中取出任务，或者调整线程的数量等。通过将 pool 
+            的状态，例如从任务队列中取出任务，或者调整线程的数量等。通过将 pool
             作为参数传递给工作线程的主函数，这个函数就可以访问和操作线程池的状态。
             而每个 Task 结构体中的函数指针和参数，只是单个任务的执行函数和参数，
             它们并不能访问和操作线程池的状态。因此，需要将 pool 作为参数传递给线
@@ -83,7 +88,7 @@ Cthreadpool* createThreadPool(int minThreadnum, int maxThreadnum, int taskmax){
             在一个无限循环中，不断从任务队列中取出任务并执行。这就是为什么我们需要
             将 pool 作为参数传递给 CreateThread 函数的原因。
             */
-            pool->ThreadWorkers[i]=CreateThread(NULL,0,ThreadWokerFunction,pool,0,&(pool->ThreadWorkersID[i]));
+            pool->ThreadWorkers[i] = CreateThread(NULL, 0, ThreadWokerFunction, pool, 0, &(pool->ThreadWorkersID[i]));
         }
 
         return pool;
@@ -95,8 +100,8 @@ Cthreadpool* createThreadPool(int minThreadnum, int maxThreadnum, int taskmax){
     那么 pool->ThreadWorker 和 pool->TaskQueue 就无法访问，这会导致未定义的
     行为，可能会引发程序崩溃。
 
-    因此，在释放 pool->ThreadWorker 和 pool->TaskQueue 之前，需要先检查 
-    pool 是否为 NULL。这就是为什么需要使用 pool && pool->ThreadWorker 和 
+    因此，在释放 pool->ThreadWorker 和 pool->TaskQueue 之前，需要先检查
+    pool 是否为 NULL。这就是为什么需要使用 pool && pool->ThreadWorker 和
     pool && pool->TaskQueue 来进行判断，而不是单纯地判断 pool->ThreadWorker
     和 pool->TaskQueue。
 
@@ -104,26 +109,27 @@ Cthreadpool* createThreadPool(int minThreadnum, int maxThreadnum, int taskmax){
     不为 NULL 时，才会尝试释放 pool->ThreadWorker 和 pool->TaskQueue 指向的
     内存。这是一种防止访问无效指针和释放未分配的内存的常见技巧。
     */
-    if(pool&&pool->ThreadWorkers){
+    if (pool && pool->ThreadWorkers)
+    {
         free(pool->ThreadWorkers);
     }
-    if(pool&&pool->TaskQueue){
+    if (pool && pool->TaskQueue)
+    {
         free(pool->TaskQueue);
     }
-    if(pool)
+    if (pool)
         free(pool);
 
     return NULL;
-    
 }
 
 DWORD ThreadWokerFunction(LPVOID laParam)
 {
-    Cthreadpool* pool=(Cthreadpool*) laParam;
+    Cthreadpool *pool = (Cthreadpool *)laParam;
     while (1)
     {
         EnterCriticalSection(&pool->mutexPool);
-        //判断队列是否为空
+        // 判断队列是否为空
         /*
         写 while 循环的目的是，工作线程被唤醒后去消费任务，消费完后导致任务队列又为空
         ，其他线程判定为空后又继续阻塞在这里
@@ -134,13 +140,14 @@ DWORD ThreadWokerFunction(LPVOID laParam)
         唤醒线程后销毁了线程池。因此，线程被唤醒后需要再次检查条件，如果条件仍然不满足
         ，那么就需要再次阻塞并等待。这就是为什么需要使用while循环的原因。
         */
-        while (pool->CurrnetTasknum==0&&!pool->IsDestoryPool)
-        {   
-            //阻塞工作线程
-            SleepConditionVariableCS(&(pool->isTaskEmpty),&(pool->mutexPool),INFINITE); 
-            
-            //判断被唤醒的线程是否需要被销毁,NeedtoKillnum在管理者线程中进行设置
-            if(pool->NeedtoKillnum>0){
+        while (pool->CurrnetTasknum == 0 && !pool->IsDestoryPool)
+        {
+            // 阻塞工作线程
+            SleepConditionVariableCS(&(pool->isTaskEmpty), &(pool->mutexPool), INFINITE);
+
+            // 判断被唤醒的线程是否需要被销毁,NeedtoKillnum在管理者线程中进行设置
+            if (pool->NeedtoKillnum > 0)
+            {
                 /*
                 pool->NeedtoKillnum-- 不能放倒kill线程的if判断中。因为假如管理者线程唤醒了
                 两次工作线程，但 “pool->liveThreadnum>pool->MinThreadnum” 条件一直没有成立
@@ -149,7 +156,8 @@ DWORD ThreadWokerFunction(LPVOID laParam)
                 线程，不管工作线程有没有自杀，都需要将 NeedtoKillnum 恢复为0。
                 */
                 pool->NeedtoKillnum--;
-                if(pool->liveThreadnum>pool->MinThreadnum){
+                if (pool->liveThreadnum > pool->MinThreadnum)
+                {
                     pool->liveThreadnum--;
                     /*
                     在Windows API中，当一个线程拥有一个临界区（也就是获取了一个临界区的锁）时，
@@ -157,30 +165,30 @@ DWORD ThreadWokerFunction(LPVOID laParam)
                     临界区。因此，如果一个线程在结束前拥有一个临界区，那么它应该在结束前释放这
                     个临界区。
 
-                    在代码中，SleepConditionVariableCS() 这行代码会在阻塞线程前自动释放 mutexPool 
+                    在代码中，SleepConditionVariableCS() 这行代码会在阻塞线程前自动释放 mutexPool
                     临界区，然后在唤醒线程后自动获取 mutexPool 临界区。因此，当线程被唤醒后，
-                    它是拥有mutexPool临界区的。所以，如果在这个线程结束前调用 ExitThread(0) 
+                    它是拥有mutexPool临界区的。所以，如果在这个线程结束前调用 ExitThread(0)
                     ，应该先调用 LeaveCriticalSection() 来释放 mutexPool 临界区。
                     */
                     LeaveCriticalSection(&pool->mutexPool);
                     goto Exit;
-                    //threadExit(pool);
+                    // threadExit(pool);
                 }
-
-            } 
+            }
         }
-        //判断线程池是否被关闭
-        if(pool->IsDestoryPool){
+        // 判断线程池是否被关闭
+        if (pool->IsDestoryPool)
+        {
             LeaveCriticalSection(&pool->mutexPool);
             break;
         }
         Task task;
-        task.functionptr=pool->TaskQueue[pool->front].functionptr;
-        task.funcargs=pool->TaskQueue[pool->front].funcargs;
-        pool->front=(pool->front+1)%pool->Taskmax;
+        task.functionptr = pool->TaskQueue[pool->front].functionptr;
+        task.funcargs = pool->TaskQueue[pool->front].funcargs;
+        pool->front = (pool->front + 1) % pool->Taskmax;
         pool->CurrnetTasknum--;
 
-        //工作线程已经消费了一个任务，通知生产线程可以继续添加任务
+        // 工作线程已经消费了一个任务，通知生产线程可以继续添加任务
         WakeConditionVariable(&pool->isTaskFull);
         LeaveCriticalSection(&pool->mutexPool);
 
@@ -200,28 +208,28 @@ DWORD ThreadWokerFunction(LPVOID laParam)
             因此，虽然多次加锁和释放锁会有一些开销，但是这样可以减少锁的持有时间，提高并发性
             能。这是一种权衡，需要根据具体的应用场景和性能需求来决定。
         */
-        printf("thread %ld starts working\n",GetCurrentThreadId());
+        printf("thread %ld starts working\n", GetCurrentThreadId());
         EnterCriticalSection(&pool->mutexWorkingnum);
         pool->WorkingThreadnum++;
         LeaveCriticalSection(&pool->mutexWorkingnum);
-        task.functionptr(task.funcargs);// 或者写为：(*task.functionptr)(task.funcargs)
-        free(task.funcargs);// 之后初始化一块堆内存，栈内存还要保证让其不被释放
-        task.funcargs=NULL;
-        printf("thread %ld end working\n",GetCurrentThreadId());
+        task.functionptr(task.funcargs); // 或者写为：(*task.functionptr)(task.funcargs)
+        free(task.funcargs);             // 之后初始化一块堆内存，栈内存还要保证让其不被释放
+        task.funcargs = NULL;
+        printf("thread %ld end working\n", GetCurrentThreadId());
         EnterCriticalSection(&pool->mutexWorkingnum);
         pool->WorkingThreadnum--;
         LeaveCriticalSection(&pool->mutexWorkingnum);
     }
-    Exit:
+Exit:
     threadExit(pool);
-
 }
 
 /*
 管理者线程按照一定的频率对线程池进行检测，看是否需要对工作线程的个数进行调节
 */
-DWORD ThreadMangerFunction(LPVOID laParam){
-    Cthreadpool* pool=(Cthreadpool*) laParam;
+DWORD ThreadMangerFunction(LPVOID laParam)
+{
+    Cthreadpool *pool = (Cthreadpool *)laParam;
 
     while (!pool->IsDestoryPool)
     {
@@ -239,124 +247,134 @@ DWORD ThreadMangerFunction(LPVOID laParam){
         如果希望你的代码能在多种环境下运行，可能需要根据环境使用条
         件编译来选择使用哪个函数
         */
-        Sleep(3000);//每隔3秒检测一次
+        Sleep(3000); // 每隔3秒检测一次
         EnterCriticalSection(&pool->mutexPool);
-        //如果没有任务，就让管理者线程休眠
-        if (pool->CurrnetTasknum == 0) {
+        // 如果没有任务，就让管理者线程休眠
+        if (pool->CurrnetTasknum == 0)
+        {
             SleepConditionVariableCS(&(pool->isTaskEmpty), &(pool->mutexPool), INFINITE);
         }
-        //取出当前任务数量和存活线程数量
+        // 取出当前任务数量和存活线程数量
         EnterCriticalSection(&pool->mutexPool);
-        int CurrentTasknum=pool->CurrnetTasknum;
-        int liveThreadnum=pool->liveThreadnum;
+        int CurrentTasknum = pool->CurrnetTasknum;
+        int liveThreadnum = pool->liveThreadnum;
         LeaveCriticalSection(&pool->mutexPool);
 
-        //取出工作线程数
+        // 取出工作线程数
         /*
         按理说简单的写法就是把工作线程数的取值放到上面与任务数量和存活
         线程数一同取值即可。因为上面是通过 mutexPool 保护了整个线程池，
         而 WorkingThreadnum是线程池中的一个成员
 
         之所以重新加锁是因为 WorkingThreadnum是线程池中的一个经常被访
-        问的成员，为了提高操作效率，因此单独给其配一把 mutexWorkingnum 
+        问的成员，为了提高操作效率，因此单独给其配一把 mutexWorkingnum
         锁。这样只需锁该变量，而不用对整个线程池上锁
         */
         EnterCriticalSection(&pool->mutexWorkingnum);
-        int WorkingThreadnum=pool->WorkingThreadnum;
-        LeaveCriticalSection(&pool->mutexWorkingnum); 
+        int WorkingThreadnum = pool->WorkingThreadnum;
+        LeaveCriticalSection(&pool->mutexWorkingnum);
 
-        //添加线程
+        // 添加线程
 
-        if(CurrentTasknum>liveThreadnum && liveThreadnum<pool->MaxThreadnum){
+        if (CurrentTasknum > liveThreadnum && liveThreadnum < pool->MaxThreadnum)
+        {
             printf("Prepare to add threads\n");
             EnterCriticalSection(&pool->mutexPool);
-            int cnt=0;//每添加线程后将cnt +1，当其等于 addThreadnum 后便不再添加
-            for(int i=0;i<pool->MaxThreadnum && cnt<PerThreadnum && pool->liveThreadnum < pool->MaxThreadnum;i++){
+            int cnt = 0; // 每添加线程后将cnt +1，当其等于 addThreadnum 后便不再添加
+            for (int i = 0; i < pool->MaxThreadnum && cnt < PerThreadnum && pool->liveThreadnum < pool->MaxThreadnum; i++)
+            {
                 /*循环添加线程的过程中，存活线程数有可能会大于最大线程数*/
 
                 /*在Windows API中，线程句柄是一个指向线程对象的指针，它的值在创建线
                 程时由系统分配。如果线程句柄为空，那么它的值应该是NULL*/
-                if(pool->ThreadWorkers[i]==NULL){
-                    pool->ThreadWorkers[i]=CreateThread(NULL,0,ThreadWokerFunction,pool,0,&pool->ThreadWorkersID[i]);
+                if (pool->ThreadWorkers[i] == NULL)
+                {
+                    pool->ThreadWorkers[i] = CreateThread(NULL, 0, ThreadWokerFunction, pool, 0, &pool->ThreadWorkersID[i]);
                     cnt++;
                     pool->liveThreadnum++;
                 }
-
             }
             LeaveCriticalSection(&pool->mutexPool);
         }
 
-        //销毁线程
-        if(WorkingThreadnum*2<liveThreadnum && liveThreadnum>pool->MinThreadnum){
-            if(WorkingThreadnum<0)
+        // 销毁线程
+        if (WorkingThreadnum * 2 < liveThreadnum && liveThreadnum > pool->MinThreadnum)
+        {
+            if (WorkingThreadnum < 0)
                 break;
             printf("Prepare to destroy thread\n");
             EnterCriticalSection(&pool->mutexPool);
-            pool->NeedtoKillnum=PerThreadnum;
+            pool->NeedtoKillnum = PerThreadnum;
             LeaveCriticalSection(&pool->mutexPool);
-            //让工作的线程自杀
+            // 让工作的线程自杀
             /*管理者线程不能直接杀死空闲线程，因为它不知道谁是空闲线程，只能通知空闲线程让它自杀*/
-            for(int i=0;i<PerThreadnum;i++){
-                /*每次使用 WakeConditionVariable() 唤醒一个和一次使用 WakeAllConditionVariable() 
+            for (int i = 0; i < PerThreadnum; i++)
+            {
+                /*每次使用 WakeConditionVariable() 唤醒一个和一次使用 WakeAllConditionVariable()
                 唤醒多个的效果是一样的。因为唤醒多个线程后，线程之间回去争抢 mutexPool ，换多个只有
                 一个可以抢到锁，也只有一个线程可以向下执行*/
                 WakeConditionVariable(&pool->isTaskEmpty);
             }
-            
         }
         LeaveCriticalSection(&pool->mutexPool);
     }
-    
 }
-//线程退出函数
-void threadExit(Cthreadpool* pool)
+// 线程退出函数
+void threadExit(Cthreadpool *pool)
 {
-    //获取当前执行到这个函数的线程的id
-    DWORD threadID= GetCurrentThreadId();
-    //遍历工作线程数组，查找执行这个函数的线程id，若匹配则kill掉
-    for(int i=0;i<pool->MaxThreadnum;i++){
-        if(pool->ThreadWorkers[i]!=NULL && GetThreadId(pool->ThreadWorkers[i])==threadID){
+    // 获取当前执行到这个函数的线程的id
+    DWORD threadID = GetCurrentThreadId();
+    // 遍历工作线程数组，查找执行这个函数的线程id，若匹配则kill掉
+    for (int i = 0; i < pool->MaxThreadnum; i++)
+    {
+        if (pool->ThreadWorkers[i] != NULL && GetThreadId(pool->ThreadWorkers[i]) == threadID)
+        {
             CloseHandle(pool->ThreadWorkers[i]);
-            pool->ThreadWorkers[i]=NULL;
-            printf("thread: %ld exiting... \n",threadID);
+            pool->ThreadWorkers[i] = NULL;
+            printf("thread: %ld exiting... \n", threadID);
             break;
         }
     }
     ExitThread(0);
 }
 
-
-//销毁线程池
-int destoryThreadPool(Cthreadpool* pool){
+// 销毁线程池
+int destoryThreadPool(Cthreadpool *pool)
+{
     printf("Destroying thread pool\n");
-    if (pool == nullptr) {
+    if (pool == nullptr)
+    {
         return 0;
     }
-    //关闭线程池
+    // 关闭线程池
     pool->IsDestoryPool = 1;
-    //唤醒所有的工作线程和管理者线程
+    // 唤醒所有的工作线程和管理者线程
     WakeAllConditionVariable(&pool->isTaskEmpty);
 
-    //等待工作线程执行结束后自毁
+    // 等待工作线程执行结束后自毁
     WaitForMultipleObjects(pool->liveThreadnum, pool->ThreadWorkers, TRUE, INFINITE);
 
     // 关闭所有线程的句柄
-    for (int i = 0; i < pool->liveThreadnum; i++) {
+    for (int i = 0; i < pool->liveThreadnum; i++)
+    {
         CloseHandle(pool->ThreadWorkers[i]);
     }
     CloseHandle(pool->ThreadManger);
-    //释放堆内存
-    if (pool->TaskQueue) {
+    // 释放堆内存
+    if (pool->TaskQueue)
+    {
         free(pool->TaskQueue);
     }
-    if (pool->ThreadWorkers) {
+    if (pool->ThreadWorkers)
+    {
         free(pool->ThreadWorkers);
     }
-    if (pool->ThreadWorkersID) {
+    if (pool->ThreadWorkersID)
+    {
         free(pool->ThreadWorkersID);
     }
-    
-    //释放互斥锁
+
+    // 释放互斥锁
     DeleteCriticalSection(&pool->mutexPool);
     DeleteCriticalSection(&pool->mutexWorkingnum);
     /*
@@ -366,68 +384,72 @@ int destoryThreadPool(Cthreadpool* pool){
     设计条件变量时，并没有为其分配资源，因此也不需要释放资源。
     */
     free(pool);
-    pool=NULL;
+    pool = NULL;
     return 1;
 }
 
-//添加任务
-void addTask(Cthreadpool* pool,void(*funcptr)(void*),void* funcarg){
+// 添加任务
+void addTask(Cthreadpool *pool, void (*funcptr)(void *), void *funcarg)
+{
     printf("Adding task to queue\n");
     EnterCriticalSection(&pool->mutexPool);
-    //任务队列满载则阻塞生产者线程
-    while (pool->CurrnetTasknum==pool->Taskmax&&!pool->IsDestoryPool)
+    // 任务队列满载则阻塞生产者线程
+    while (pool->CurrnetTasknum == pool->Taskmax && !pool->IsDestoryPool)
     {
-        SleepConditionVariableCS(&pool->isTaskFull,&pool->mutexPool,INFINITE);
+        SleepConditionVariableCS(&pool->isTaskFull, &pool->mutexPool, INFINITE);
     }
-    if(pool->IsDestoryPool)
+    if (pool->IsDestoryPool)
     {
         LeaveCriticalSection(&pool->mutexPool);
         return;
     }
-    //无需堆满检查，因为 while 循环中已经保证了队列有空位。
-    pool->TaskQueue[pool->rear].functionptr=funcptr;
-    pool->TaskQueue[pool->rear].funcargs=funcarg;
+    // 无需堆满检查，因为 while 循环中已经保证了队列有空位。
+    pool->TaskQueue[pool->rear].functionptr = funcptr;
+    pool->TaskQueue[pool->rear].funcargs = funcarg;
     pool->CurrnetTasknum++;
-    pool->rear=(pool->rear+1)%pool->Taskmax;
+    pool->rear = (pool->rear + 1) % pool->Taskmax;
 
-    //生产者线程添加了任务，唤醒工作线程去消费。同时，也会唤醒因任务数为空而阻塞的管理者线程
+    // 生产者线程添加了任务，唤醒工作线程去消费。同时，也会唤醒因任务数为空而阻塞的管理者线程
     WakeConditionVariable(&pool->isTaskEmpty);
     LeaveCriticalSection(&pool->mutexPool);
 }
 
-//获取工作的线程个数
-int getWorkingThreadnum(Cthreadpool* pool){
+// 获取工作的线程个数
+int getWorkingThreadnum(Cthreadpool *pool)
+{
     EnterCriticalSection(&pool->mutexPool);
-    int WorkingThreadnum=pool->WorkingThreadnum;
+    int WorkingThreadnum = pool->WorkingThreadnum;
     LeaveCriticalSection(&pool->mutexPool);
     return WorkingThreadnum;
 }
 
-//获取存活线程数
-int getLiveThreadnum(Cthreadpool* pool){
+// 获取存活线程数
+int getLiveThreadnum(Cthreadpool *pool)
+{
     EnterCriticalSection(&pool->mutexPool);
-    int LiveThreadnum=pool->liveThreadnum;
+    int LiveThreadnum = pool->liveThreadnum;
     LeaveCriticalSection(&pool->mutexPool);
     return LiveThreadnum;
 }
 
-
-void taskFunc(void* arg){
-    int num=*(int*)arg;
-    printf("thread %ld is doing something....,num=%d\n",GetCurrentThreadId(),num);
+void taskFunc(void *arg)
+{
+    int num = *(int *)arg;
+    printf("thread %ld is doing something....,num=%d\n", GetCurrentThreadId(), num);
     sleep(1);
 }
 
 int main()
 {
-    Cthreadpool* pool= createThreadPool(3,10,100);
+    Cthreadpool *pool = createThreadPool(3, 10, 100);
 
-    for(int i=0;i<100;++i){
-        int* num=(int*)malloc(sizeof(int));
-        *num=i+100;
-        addTask(pool,taskFunc,num);
+    for (int i = 0; i < 100; ++i)
+    {
+        int *num = (int *)malloc(sizeof(int));
+        *num = i + 100;
+        addTask(pool, taskFunc, num);
     }
-    //销毁线程之前，工作线程有可能没有执行完毕，因此让主线程睡眠一段时间
+    // 销毁线程之前，工作线程有可能没有执行完毕，因此让主线程睡眠一段时间
 
     sleep(30);
     destoryThreadPool(pool);
--- Cthreadpool.h
+++ Cthreadpool.h
@@ -1,42 +1,41 @@
-#include<windows.h>
-//任务结构体
+#include <windows.h>
+// 任务结构体
 typedef struct Task
 {
     /* data */
-    void (*functionptr)(void* funcargs);
-    void* funcargs;
-}Task;
+    void (*functionptr)(void *funcargs);
+    void *funcargs;
+} Task;
 
-//线程池结构体
+// 线程池结构体
 struct Cthreadpool
 {
     /* Task Queue */
-    //操作任务队列及相关数据时，使用mutex锁来保证互斥
-    Task* TaskQueue;
-    int Taskmax;//最大容量
-    int CurrnetTasknum;//当前任务容量
-    int front;//对头
-    int rear;//d队尾
+    // 操作任务队列及相关数据时，使用mutex锁来保证互斥
+    Task *TaskQueue;
+    int Taskmax;        // 最大容量
+    int CurrnetTasknum; // 当前任务容量
+    int front;          // 对头
+    int rear;           // d队尾
 
+    HANDLE ThreadManger;              // 管理者线程句柄
+    HANDLE *ThreadWorkers;            // 工作线程句柄
+    DWORD ThreadMangerID;             // 管理者线程标识符
+    DWORD *ThreadWorkersID;           // 工作线程标识符
+    int MaxThreadnum;                 // 最大线程数
+    int MinThreadnum;                 // 最小线程数
+    int WorkingThreadnum;             // 当前工作线程数----------->灵活变量，需要考虑单独上锁
+    int liveThreadnum;                // 存活线程数
+    int NeedtoKillnum;                // 需要杀死的线程个数（当存活个数大于工作个数时）
+    CRITICAL_SECTION mutexPool;       // 整个线程池的锁
+    CRITICAL_SECTION mutexWorkingnum; // 单独对 WorkingThreadnum 上锁
 
-    HANDLE ThreadManger;//管理者线程句柄
-    HANDLE* ThreadWorkers;//工作线程句柄
-    DWORD ThreadMangerID;//管理者线程标识符
-    DWORD* ThreadWorkersID;//工作线程标识符
-    int MaxThreadnum;//最大线程数
-    int MinThreadnum;//最小线程数
-    int WorkingThreadnum;//当前工作线程数----------->灵活变量，需要考虑单独上锁
-    int liveThreadnum;//存活线程数
-    int NeedtoKillnum;//需要杀死的线程个数（当存活个数大于工作个数时）
-    CRITICAL_SECTION mutexPool;//整个线程池的锁
-    CRITICAL_SECTION mutexWorkingnum;//单独对 WorkingThreadnum 上锁
-
-    int IsDestoryPool; //是否要销毁线程池,销毁为1，否则为0
-    CONDITION_VARIABLE isTaskFull; //任务队列是否已满--->生产者条件变量
-    CONDITION_VARIABLE isTaskEmpty; //任务队列是否为空--->消费者条件变量
+    int IsDestoryPool;              // 是否要销毁线程池,销毁为1，否则为0
+    CONDITION_VARIABLE isTaskFull;  // 任务队列是否已满--->生产者条件变量
+    CONDITION_VARIABLE isTaskEmpty; // 任务队列是否为空--->消费者条件变量
 };
 
-const int PerThreadnum=2;//每次 添加\销毁 的线程数
+const int PerThreadnum = 2; // 每次 添加\销毁 的线程数
 
 /*
 在Windows的多线程编程中，使用 CreateThread() 函数创建线程时，传入
@@ -44,7 +43,7 @@ const int PerThreadnum=2;//每次 添加\销毁 的线程数
 接受一个 LPVOID 类型的参数。这是Windows API的要求
 DWORD WINAPI ThreadFunction(LPVOID lpParam);
 
-在POSIX线程（pthread）中，线程函数的要求稍有不同。在 pthread_create() 
+在POSIX线程（pthread）中，线程函数的要求稍有不同。在 pthread_create()
 函数中，线程函数必须返回 void* 类型，并且接受一个 void* 类型的参数
 void* ThreadFunction(void* arg);
 
@@ -54,12 +53,11 @@ void* ThreadFunction(void* arg);
 DWORD ThreadMangerFunction(LPVOID laParam);
 DWORD ThreadWokerFunction(LPVOID lpParam);
 
-
-Cthreadpool* createThreadPool(int minThreadnum, int maxThreadnum, int taskmax);
+Cthreadpool *createThreadPool(int minThreadnum, int maxThreadnum, int taskmax);
 DWORD ThreadWokerFunction(LPVOID laParam);
 DWORD ThreadMangerFunction(LPVOID laParam);
-void threadExit(Cthreadpool* pool);
-int destroyThreadPool(Cthreadpool* pool);
-void addTask(Cthreadpool* pool,void(*funcptr)(void*),void* funcarg);
-int getWorkingThreadnum(Cthreadpool* pool);
-int getLiveThreadnum(Cthreadpool* pool);
+void threadExit(Cthreadpool *pool);
+int destroyThreadPool(Cthreadpool *pool);
+void addTask(Cthreadpool *pool, void (*funcptr)(void *), void *funcarg);
+int getWorkingThreadnum(Cthreadpool *pool);
+int getLiveThreadnum(Cthreadpool *pool);
